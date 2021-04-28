841066de787ca76054b92c0c55d44d6ce6481d49
Sabin Regmi <xawbeenregmi@gmail.com>
2021-04-29 00:39:52 +0545

    Recent Order
    -- Added recent orders
    -- added bouncy effect in listview

--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -5,12 +5,87 @@ import 'package:food_delivery_app_ui/models/order.dart';
 class RecentOrders extends StatelessWidget {
   const RecentOrders({Key key}) : super(key: key);
 
-  _buildRecentOrder(BuildContext context, int index) {
+  _buildRecentOrder(BuildContext context, Order order) {
     return Container(
       margin: EdgeInsets.all(10.0),
       width: 320.0,
       decoration: BoxDecoration(
-        color: Colors.white,
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(
+            width: 1.0,
+            color: Colors.grey[200],
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: Row(children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image(
+                  height: 100.0,
+                  width: 100.0,
+                  image: AssetImage(order.food.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        order.food.name,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4.0,
+                      ),
+                      Text(
+                        order.restaurant.name,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4.0,
+                      ),
+                      Text(
+                        order.date,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20.0),
+            width: 48.0,
+            decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(30.0)),
+            child: IconButton(
+              icon: Icon(Icons.add),
+              iconSize: 30.0,
+              color: Colors.white,
+              onPressed: () {},
+            ),
+          )
+        ],
       ),
     );
   }
@@ -33,13 +108,14 @@ class RecentOrders extends StatelessWidget {
         ),
         Container(
           height: 120,
-          color: Colors.blue,
           child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              padding: EdgeInsets.only(left: 10.0),
               scrollDirection: Axis.horizontal,
               itemCount: currentUser.orders.length,
               itemBuilder: (BuildContext ctx, int index) {
                 Order order = currentUser.orders[index];
-                return _buildRecentOrder(ctx, index);
+                return _buildRecentOrder(ctx, order);
               }),
         )
       ],
